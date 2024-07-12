0a18dff51f49e4c12b1a78c06120d1fd17b45904
Lucas Crownover <lcrownover127@gmail.com>
2024-07-12 11:15:39 -0700

    state refactor

--- internal/slurm/parsers.go
+++ internal/slurm/parsers.go
@@ -17,6 +17,21 @@ const (
 	JobStateUnknown
 )
 
+type NodeState int
+
+const (
+	NodeStateAlloc NodeState = iota
+	NodeStateComp
+	NodeStateDown
+	NodeStateDrain
+	NodeStateFail
+	NodeStateErr
+	NodeStateIdle
+	NodeStateMaint
+	NodeStateMix
+	NodeStateResv
+)
+
 // GetJobAccountName retrieves the account name string from the JobInfo object or returns error
 func GetJobAccountName(job types.V0040JobInfo) (*string, error) {
 	name := job.Account
@@ -28,7 +43,6 @@ func GetJobAccountName(job types.V0040JobInfo) (*string, error) {
 
 // GetJobState returns a JobState unit or returns an error
 func GetJobState(job types.V0040JobInfo) (*JobState, error) {
-	// job state should be a list of strings, but the spec is []interface{}
 	states := job.JobState
 	if states == nil {
 		// job state is not found in the job response
@@ -65,4 +79,55 @@ func GetJobCPUs(job types.V0040JobInfo) (*float64, error) {
 	}
 	cpus := float64(*cn)
 	return &cpus, nil
+}
+
+// GetNodeState returns a NodeState unit or returns an error
+func GetNodeState(node types.V0040Node) (*NodeState, error) {
+	states := node.State
+	if states == nil {
+		// node state is not found in the node response
+		return nil, fmt.Errorf("node state not found in node")
+	}
+	state := (*states)[0].(string)
+	state = strings.ToLower(state)
+
+	alloc := regexp.MustCompile(`^alloc`)
+	comp := regexp.MustCompile(`^comp`)
+	down := regexp.MustCompile(`^down`)
+	drain := regexp.MustCompile(`^drain`)
+	fail := regexp.MustCompile(`^fail`)
+	err := regexp.MustCompile(`^err`)
+	idle := regexp.MustCompile(`^idle`)
+	maint := regexp.MustCompile(`^maint`)
+	mix := regexp.MustCompile(`^mix`)
+	resv := regexp.MustCompile(`^res`)
+
+	var stateUnit NodeState
+
+	switch {
+	case alloc.MatchString(state):
+		stateUnit = NodeStateAlloc
+	case comp.MatchString(state):
+		stateUnit = NodeStateComp
+	case down.MatchString(state):
+		stateUnit = NodeStateDown
+	case drain.MatchString(state):
+		stateUnit = NodeStateDrain
+	case fail.MatchString(state):
+		stateUnit = NodeStateFail
+	case err.MatchString(state):
+		stateUnit = NodeStateErr
+	case idle.MatchString(state):
+		stateUnit = NodeStateIdle
+	case maint.MatchString(state):
+		stateUnit = NodeStateMaint
+	case mix.MatchString(state):
+		stateUnit = NodeStateMix
+	case resv.MatchString(state):
+		stateUnit = NodeStateResv
+	default:
+		return nil, fmt.Errorf("failed to match job state against known states: %v", state)
+	}
+
+	return &stateUnit, nil
 }
