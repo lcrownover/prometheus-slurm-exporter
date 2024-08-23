71da313630b0a8225dcd09d28251eaa2e642b2b5
Lucas Crownover <lcrownover127@gmail.com>
2024-08-23 14:32:39 -0700

    fmt

--- internal/slurm/accounts_test.go
+++ internal/slurm/accounts_test.go
@@ -10,7 +10,7 @@ import (
 func TestParseAccountMetrics(t *testing.T) {
 	fb := util.ReadTestDataBytes("V0040OpenapiJobInfoResp.json")
 	jobsResp, _ := api.UnmarshalJobsResponse(fb)
-	_, err := ParseAccountsMetrics(jobsResp.Jobs)	
+	_, err := ParseAccountsMetrics(jobsResp.Jobs)
 	if err != nil {
 		t.Fatalf("failed to parse account metrics: %v\n", err)
 	}
