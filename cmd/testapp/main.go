2d2ddd446a07a982a3b1dadb7d340be72edfd494
Lucas Crownover <lcrownover127@gmail.com>
2024-09-19 15:15:46 -0700

    more test

--- cmd/testapp/main.go
+++ cmd/testapp/main.go
@@ -9,14 +9,27 @@ import (
 )
 
 func main() {
+	fmt.Println("trying api json")
 	b, err := os.ReadFile("/tmp/shares-api.json")
 	if err != nil {
 		fmt.Printf("failed to read file: %v\n", err)
 	}
-	var r types.V0040OpenapiSharesResp
-	err = json.Unmarshal(b, &r)
+	var ra types.V0040OpenapiSharesResp
+	err = json.Unmarshal(b, &ra)
 	if err != nil {
 		fmt.Printf("failed to unmarshal json: %v\n", err)
 	}
-	fmt.Printf("%+v\n", r)
+	fmt.Printf("%+v\n", ra)
+
+	fmt.Println("trying copied json")
+	b, err = os.ReadFile("/tmp/shares-copy.json")
+	if err != nil {
+		fmt.Printf("failed to read file: %v\n", err)
+	}
+	var rc types.V0040OpenapiSharesResp
+	err = json.Unmarshal(b, &rc)
+	if err != nil {
+		fmt.Printf("failed to unmarshal json: %v\n", err)
+	}
+	fmt.Printf("%+v\n", rc)
 }
