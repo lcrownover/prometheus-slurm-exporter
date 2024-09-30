6b9495c1d2b3445b5babe2fd5b4129eec95190ce
Lucas Crownover <lcrownover127@gmail.com>
2024-09-30 08:51:29 -0700

    fixed lack of Infinity parser in 2311 and added Infinity to test data

--- internal/api/unmarshalers_2311.go
+++ internal/api/unmarshalers_2311.go
@@ -5,6 +5,7 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	openapi "github.com/lcrownover/openapi-slurm-23-11"
 )
@@ -51,10 +52,28 @@ func UnmarshalPartitionsResponse(b []byte) (*openapi.V0040OpenapiPartitionResp, 
 
 // UnmarshalSharesResponse converts the response bytes into a slurm type
 func UnmarshalSharesResponse(b []byte) (*openapi.V0040OpenapiSharesResp, error) {
+	// this is disgusting but the response has values of "Infinity" which are
+	// not json unmarshal-able, so I manually replace all the "Infinity"s with the correct
+	// float64 value that represents Infinity.
+	// this will be fixed in v0.0.42
+	// https://support.schedmd.com/show_bug.cgi?id=20817
+	//
+	// https://github.com/lcrownover/prometheus-slurm-exporter/issues/8
+	// also reported that folks are getting "inf" back, so I'll protect for that too
+	sharesRespString := string(b)
+	maxFloatStr := ": 1.7976931348623157e+308"
+	sharesRespString = strings.ReplaceAll(sharesRespString, ": Infinity", maxFloatStr) // replacing the longer strings first should prevent any partial replacements
+	sharesRespString = strings.ReplaceAll(sharesRespString, ": infinity", maxFloatStr)
+	sharesRespString = strings.ReplaceAll(sharesRespString, ": Inf", maxFloatStr) // sometimes it'd return "inf", so let's cover for that too.
+	sharesRespString = strings.ReplaceAll(sharesRespString, ": inf", maxFloatStr)
+	sharesRespBytes := []byte(sharesRespString)
+	// end hack
+
 	var sharesResp openapi.V0040OpenapiSharesResp
-	err := json.Unmarshal(b, &sharesResp)
+	err := json.Unmarshal(sharesRespBytes, &sharesResp)
 	if err != nil {
 		return nil, fmt.Errorf("failed to unmarshall shares response data: %v", err)
 	}
+
 	return &sharesResp, nil
 }
